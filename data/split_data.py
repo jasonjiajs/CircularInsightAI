3574856f513ec7b3d4e84cbe39f97f218cd0f7b4
Nuobei <90873043+nuobeiz@users.noreply.github.com>
2024-01-07 16:05:17 -0500

    Update split_data.py

--- data/split_data.py
+++ data/split_data.py
@@ -7,4 +7,4 @@
 train_data, test_data = train_test_split(data, test_size=100, random_state=42)
 # Save train and test sets to new CSV files
 train_data.to_csv('train_data.csv', index=False)
-test_data.to_csv('test_data.csv', index=False)
+test_data.to_csv('validation_and_test_data.csv', index=False)
